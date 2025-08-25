069b22e8d350b2717c429b5edaaaa904cd19ce1a
nicoa <nicoalba10@gmail.com>
2025-08-24 21:41:11 -0600

    add  comment to intro

--- solana-test-app/programs/solana-test-app/src/lib.rs
+++ solana-test-app/programs/solana-test-app/src/lib.rs
@@ -1,5 +1,9 @@
 use anchor_lang::prelude::*;
 
+/* This is a very basic Rust hello-world program for Solana using the Anchor framework. It doesn't stsore any state
+state or perform any complex operation; it simply logs a greeting message when the `initialize` function is called.
+*/
+
 declare_id!("HD3sxGps2pr36KvHZY4JjaQgG9otncF9SCovGvTvhvdp");
 
 #[program]
