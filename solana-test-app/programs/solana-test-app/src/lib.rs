908f5852388cabd8d65317399a272cab293b36b4
nicoa <nicoalba10@gmail.com>
2025-08-28 13:01:39 -0600

    add notes to hello world app

--- solana-test-app/programs/solana-test-app/src/lib.rs
+++ solana-test-app/programs/solana-test-app/src/lib.rs
@@ -4,17 +4,17 @@ use anchor_lang::prelude::*;
 state or perform any complex operation; it simply logs a greeting message when the `initialize` function is called.
 */
 
-declare_id!("EqYLJzQSwpqLa1ByR43TjARd8sxEsyaYnM8mGEGAWmg1");
+declare_id!("EqYLJzQSwpqLa1ByR43TjARd8sxEsyaYnM8mGEGAWmg1"); // program ID
 
-#[program]
-pub mod solana_test_app {
-    use super::*;
+#[program] // module declaration
+pub mod solana_test_app { // public module named solana_test_app
+    use super::*; // imports all items from the parent module
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> { // defines a public function that takes a context parameter of type `Context<Initialize>` and returns a `Result<()>`.
+        msg!("Greetings from: {:?}", ctx.program_id); // logs a greeting message that includes the program ID
+        Ok(())  // indicates successful completion of the function
     }
 }
 
-#[derive(Accounts)]
-pub struct Initialize {}
+#[derive(Accounts)] // derives the `Accounts` trait for the `Initialize` struct
+pub struct Initialize {} // defines an empty struct named `Initialize` that serves as a context for the `initialize` function
